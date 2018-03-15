7d25e874afc38f43bffcc36e75b451ebb5f92eb8
atul rajsheel <rajsheel@gmail.com>
2018-03-14 21:48:56 -0400

    removed unnecessary code

--- sample.js
+++ sample.js
@@ -1,18 +1,4 @@
-/*
- * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-**/
+
 
 // Load the SDK and UUID
 var AWS = require('aws-sdk');
@@ -21,9 +7,7 @@ AWS.config.update({region: 'us-east-1'});
 // Create an S3 client
 var s3 = new AWS.S3();
 
-// Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+
 
 // Call S3 to list current buckets
 s3.listBuckets(function(err, data) {
@@ -46,14 +30,3 @@ s3.createBucket(bucketParams, function(err, data) {
       console.log("Success", data.Location);
    }
 });
-/*
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-  });
-});
-*/
